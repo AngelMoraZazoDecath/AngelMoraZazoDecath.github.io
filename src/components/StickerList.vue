59d39c66bb605eb740dde848ccd92867bbe0dc87
AngelMoraZazoDecath <83016240+AngelMoraZazoDecath@users.noreply.github.com>
2022-07-13 16:29:47 +0200

    disable list when click answer and put new font

--- src/components/StickerList.vue
+++ src/components/StickerList.vue
@@ -1,48 +1,50 @@
 <template>
-    <div class="options-container">
-        <ul>
-            <li v-for="sticker in stickers" :key="sticker.id" @click="$emit('selection', sticker.id)">
-                {{ sticker.name }}
-            </li>
-        </ul>
-    </div>
+  <div class="options-container">
+    <ul>
+      <li
+        v-for="sticker in stickers"
+        :key="sticker.id"
+        @click="$emit('selection', sticker.id,) "
+      >
+        {{ sticker.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        stickers: {
-            type: Array,
-            required: true
-        }
-    }
-}
+  props: {
+    stickers: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-
-
 ul {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 li {
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-    margin-bottom: 10px;
-    width: 250px;
-    font-family: 'Press Start 2P', cursive;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+  font-family: "Press Start 2P", cursive;
+  font-size: 12px;
 }
 
 li:hover {
-    background-color: rgba(0, 0, 0, 0.05);
+  background-color: rgba(0, 0, 0, 0.05);
 }
 
 .options-container {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
--- src/pages/StickersPage.vue
+++ src/pages/StickersPage.vue
@@ -9,12 +9,11 @@
             <h1>Acumula 20 vidas para ganar la partida! </h1><br>
             <h1>¿De quien es este sticker?</h1>
             <StickerImage :stickerId="sticker.id" :showSticker="showStickers" :imagen="sticker.urlSticker" :pista ="sticker.pistaData" />
-            <StickerList :stickers="stickersArray" @selection="checkAnswer($event)" />
+            <StickerList v-if="cnt < 1" :stickers="stickersArray" @selection="checkAnswer($event)"/>
             <div style="display: flex; justify-content: center;">
-            <button class="vtmn-btn vtmn-btn_variant--primary vtmn-btn_size--medium" id="buttonPista" v-if="!respuesta" @click="askPista()">Pedir pista!</button>
+            <button class="vtmn-btn vtmn-btn_variant--primary vtmn-btn_size--medium" id="buttonPista" v-if="!respuesta" @click="askPista()" >Pedir pista!</button>
             </div>
             <div class="vtmn-card" id="pista">{{pistaToShow}}</div>
-                 
             <template v-if="respuesta">
                 <div id="respuesta">{{respuesta}}</div>
                 <div style="display: flex; justify-content: center">
@@ -45,7 +44,8 @@ export default {
             showStickers: false,
             aciertos:null,
             pistaToShow:null,
-            endMessage:null
+            endMessage:null,
+            cnt:0
         }
 
     },
@@ -59,10 +59,13 @@ export default {
                 this.showStickers = true
                 this.pistaToShow = null 
             if (stickerId == this.sticker.id) {
-                    this.respuesta = " CORRECTO!", this.aciertos++ } 
+                    this.respuesta = " CORRECTO!",
+                    this.aciertos++,
+                    this.cnt++ } 
                 else { 
                     this.respuesta=`Lo siento. Es ${this.sticker.name}` ,
-                    this.aciertos-- 
+                    this.aciertos-- ,
+                    this.cnt++
             } 
             if(this.aciertos >19){
                 this.respuesta = false
@@ -81,6 +84,7 @@ export default {
             this.respuesta=null,
             this.mixStickerArray(), 
             this.pistaToShow = null 
+            this.cnt=0
         },
         
         askPista(){
@@ -99,22 +103,29 @@ export default {
 </script>
 <style>
 
-@import url('http://fonts.cdnfonts.com/css/arcade-classic');
+
+
+
+
+
+
+
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 
 #respuesta{
-    font-size: 30px;
-     font-family: 'ArcadeClassic', sans-serif;
-        transform: translateX(calc((-100vw + 100%)/ 2));
+    font-size: 12px;
+    font-family: 'Press Start 2P', cursive;
+    transform: translateX(calc((-100vw + 100%)/ 2));
 }
 
 #vidas {
-    font-family: 'ArcadeClassic', sans-serif;
-    font-size: 30px;
+    font-family: 'Press Start 2P', cursive;
+    font-size: 15px;
     color: red;
     transform: translateX(calc((-100vw + 100%)/ 2));
 }
 h1{
-    font-family: 'ArcadeClassic', sans-serif;
+    font-family: 'Press Start 2P', cursive;
         transform: translateX(calc((-100vw + 100%)/ 2));
 }
 #buttonPista{
@@ -124,14 +135,20 @@ h1{
 }
    
 #pista{
-    font-family: 'Verdana', sans-serif;
+    font-family: 'Press Start 2P', cursive;
     display: flex; 
     justify-content: center;
     clear: both;
     width: auto;
-    margin-left:25%;
-    margin-right:25%;
-    margin-top:1%
+    margin: 1% 25% 0% 25%;
+    font-size: 12px;
+    color:#007CBD;
+}
+
+#end{
+    font-family: 'Press Start 2P', cursive;
+    font-size: 15px;
+    color: red;
 }
 
 
