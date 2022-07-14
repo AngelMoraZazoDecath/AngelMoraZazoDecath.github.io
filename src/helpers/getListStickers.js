f356fed609aa6c56a4670542fff0e01e1559ec51
AngelMoraZazoDecath <83016240+AngelMoraZazoDecath@users.noreply.github.com>
2022-07-14 18:39:22 +0200

    Update getListStickers.js

--- src/helpers/getListStickers.js
+++ src/helpers/getListStickers.js
@@ -12,8 +12,18 @@ const getListStickers = async () => {
     const randomArray = getSticker().sort(() => Math.random() - 0.5)
     const stickers = await getStickerName(randomArray.splice(0, 4))
     console.log(stickers)
+    const values = stickers.map((sticker) => sticker.name)
+    console.log(values)
+    const nameDuplicates = async () => {
+        if (values.length !== new Set(values).size) {
+            return getListStickers()
+        } else {
+            return stickers
+        }
+    }
 
-    return stickers
+    return await nameDuplicates()
+
 
 }
 
@@ -28,17 +38,13 @@ const getStickerName = async ([a, b, c, d] = randomArray) => {
     ]
     const [p1, p2, p3, p4] = await Promise.all(promiseArray).then()
 
-    const arrayStrickers = [p2.data.name, p3.data.name, p4.data.name]
-    if (!arrayStrickers.includes(p1.data.name)) {
-        getListStickers();
-    }
-
     return [
         { name: p1.data.name, id: p1.data.id, urlSticker: p1.data.urlSticker, pistaData: p1.data.pistaData },
         { name: p2.data.name, id: p2.data.id, urlSticker: p2.data.urlSticker, pistaData: p2.data.pistaData },
         { name: p3.data.name, id: p3.data.id, urlSticker: p3.data.urlSticker, pistaData: p3.data.pistaData },
         { name: p4.data.name, id: p4.data.id, urlSticker: p4.data.urlSticker, pistaData: p4.data.pistaData }
     ]
+
 
 
 }
